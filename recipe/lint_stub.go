ee7a92509ba0a9c90bc6af340814ed2710f9131c
yangyifan18 <xcnryyf@163.com>
2026-05-21 18:54:52 +0800

    chore: remove temporary lint stub

--- recipe/lint_stub.go
+++
@@ -1,9 +0,0 @@
-package recipe
-
-type LintResult struct {
-	Findings []LintFinding `json:"findings"`
-}
-
-func LintArchive(path string, opts LintOptions) (LintResult, error) {
-	return LintResult{}, nil
-}
